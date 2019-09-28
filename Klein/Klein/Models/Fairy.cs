0be35599c90c034ca8697acf69d6128c0e245cbf
visual_community@outlook.com <visual_community@outlook.com>
2019-09-28 09:33:11 -0400

    Segundo Commit

---
+++ Klein/Klein/Models/Fairy.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Klein.Models
+{
+    public enum TypeList {
+
+        Luna,
+        Luz,
+        Lima
+    }
+
+    public class Fairy
+    {
+        [Key]
+        public int FairyID { get; set; }
+
+        [Required]
+        public string NickName { get; set; }
+
+        [Required]
+        [Display(Name="Nombre Completo")]
+        [StringLength(24,MinimumLength =2)]
+        public TypeList Wishes { get; set; }
+
+        [DataType(DataType.EmailAddress, ErrorMessage = "Direccion de correo no valida")]
+        public string Email { get; set; }
+
+        [Display(Name="Cumpleaños")]
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:dd-MM-yyyy}", ApplyFormatInEditMode = true)]
+        public DateTime Birthdate { get; set; }
+
+    }
+}
